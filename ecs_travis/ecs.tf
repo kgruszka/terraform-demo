22e35c94d1f3c293be601029a166508357479afe
kgruszka <kamilgruszka1993.kg@gmail.com>
2018-10-30 16:42:56 +0100

    remote module

--- ecs_travis/ecs.tf
+++ ecs_travis/ecs.tf
@@ -1,6 +1,6 @@
 module "ecs" {
-    # source = "github.com/kgruszka/terraform-demo.git//ecs_module?ref=v0.0.1"
-    source = "../ecs_module"
+    source = "github.com/kgruszka/terraform-demo.git//ecs_module?ref=v0.0.1"
+    # source = "../ecs_module"
 
     cluster_name = "${var.cluster_name}"
     cluster_key_name = "${var.cluster_key_name}"
